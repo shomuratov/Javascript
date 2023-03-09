b3488c3c722b09c2f815a78441287e625c20ddf9
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-03-09 16:55:21 +0500

    Update greet_me.js

--- 7kyu/greet_me.js
+++ 7kyu/greet_me.js
@@ -1,1 +1,7 @@
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
 
+let greet = function(name) {
+    return console.log(`Hello ${(name.slice(0,1).toUpperCase())+(name.slice(1).toLowerCase())}!`)
+};
+
+greet('Azamat')
